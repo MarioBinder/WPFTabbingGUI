555c8b2a9f7cc1f316875b99c1a3ed62853e4fdd
sx <mp@biggle.de>
2010-08-08 00:07:43 +0200

    delete obj subfolder

--- WPFTabbingGUI/Common/ViewModelBase.cs
+++ WPFTabbingGUI/Common/ViewModelBase.cs
@@ -7,33 +7,33 @@ using System.Linq.Expressions;
 
 namespace WPFTabbingGUI.Common
 {
-    /// <summary>
-    /// the viewmodelbase implemented INotifyPropertyChanged
-    /// with type-save INotifyProperty
-    /// idea from jp.hamilton: http://www.jphamilton.net/blog/post/MVVM-with-Type-Safe-INotifyPropertyChanged.aspx
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class ViewModelBase<T> : INotifyPropertyChanged
+/// <summary>
+/// the viewmodelbase implemented INotifyPropertyChanged
+/// with type-save INotifyProperty
+/// idea from jp.hamilton: http://www.jphamilton.net/blog/post/MVVM-with-Type-Safe-INotifyPropertyChanged.aspx
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class ViewModelBase<T> : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected void RaisePropertyChanged<R>(Expression<Func<T, R>> x)
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        var body = x.Body as MemberExpression;
+        if (body == null)
+            throw new ArgumentException("'x' should be a member expression");
 
-        protected void RaisePropertyChanged<R>(Expression<Func<T, R>> x)
+        string propertyName = body.Member.Name;
+
+        PropertyChangedEventHandler handler = this.PropertyChanged;
+
+        if (handler != null)
         {
-            var body = x.Body as MemberExpression;
-            if (body == null)
-                throw new ArgumentException("'x' should be a member expression");
-
-            string propertyName = body.Member.Name;
-
-            PropertyChangedEventHandler handler = this.PropertyChanged;
-
-            if (handler != null)
-            {
-                var e = new PropertyChangedEventArgs(propertyName);
-                handler(this, e);
-            }
+            var e = new PropertyChangedEventArgs(propertyName);
+            handler(this, e);
         }
     }
+}
 }
 
 
